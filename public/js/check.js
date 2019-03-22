4cc14cacd72f4752d29c82ddf0a1de5b42201a2b
Steven Gao <stevenruidigao@gmail.com>
2019-03-22 17:02:51 -0400

    Remove unnecessary check

--- public/js/check.js
+++ public/js/check.js
@@ -1,7 +1,5 @@
 // var canvas = document.getElementById('canvas');
 // canvas.hidden = false;
-var p = document.getElementById('enableJS');
-p.hidden = true;
 // canvas.width = document.body.clientWidth;
 // canvas.height = document.body.clientHeight - 50;
 // var ctx = canvas.getContext("2d");
